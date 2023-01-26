d04ee2c913294c3b1eac629b6db47cb443813335
kamszeliga <kamrynszeliga@gmail.com>
2023-01-26 17:36:06 -0500

    new logo

--- js/site.js
+++ js/site.js
@@ -15,7 +15,7 @@ function getValues() {
         let numbersArray = generateNumbers(fizzValue, buzzValue, endValue);
         //& display on page
         displayNumbers(numbersArray);
-    } if (endValue > 5000){
+    } if (endValue > 5000) {
         //IF NO - tell user they goofed
         Swal.fire(
             {
@@ -24,75 +24,76 @@ function getValues() {
                 text: 'That number is waaaay too high! Please enter a value below 5000 for the stop value.',
             }
         );
-    // } else {
-    //     //IF NO - tell user they goofed, numbers only 
-    //     Swal.fire(
-    //         {
-    //             icon: 'error',
-    //             title: 'You goofed it...',
-    //             text: 'Only integers are allowed for this input.',
-    //         }
-    //     );
-    // } 
-    
+        // } else {
+        //     //IF NO - tell user they goofed, numbers only 
+        //     Swal.fire(
+        //         {
+        //             icon: 'error',
+        //             title: 'You goofed it...',
+        //             text: 'Only integers are allowed for this input.',
+        //         }
+        //     );
+        // } 
 
-}
 
-//generate the numbers within the specified value, stopping at the stop value
-function generateNumbers(fizzValue, buzzValue, endValue) {
-    let numbers = [];
-    let end = endValue;
-
-    for (let value = 1; value <= end; value++) {
-        let newValue = value;
-
-        if (value % fizzValue == 0) {
-            newValue = 'Fizz'
-        }
-        
-        if (value % buzzValue == 0) {
-            newValue = 'Buzz'
-        }
-        
-        if (value % buzzValue == 0 && value % fizzValue == 0) {
-            newValue = 'FizzBuzz'
-        }
-
-        numbers.push(newValue);
-    }
-    return numbers;
-
-}
-
-//Turns the table appearance into fizz, buzz and fizz buzz
-function displayNumbers(numbersArray) {
-    let tableBody = document.getElementById('results');
-
-    let tableHtml = "";
-
-    for (let index = 0; index < numbersArray.length; index++) {
-        let value = numbersArray[index];
-        let className = '';
-        if (value == 'Fizz') {
-            className = 'fizz';
-        }
-
-        if (value == 'Buzz') {
-            className = 'buzz';
-        }
-
-        if (value == 'FizzBuzz') {
-            className = 'fizzbuzz';
-        }
-
-        let tableRow = `<td class="${className}">${value}</td>`;
-
-        tableHtml = tableHtml + tableRow;
-
-        if ((index + 1) % 5 == 0) {
-            tableHtml += '</tr>';
-        }
     }
 
-    tableBody.innerHTML = tableHtml;
-        }}
+    //generate the numbers within the specified value, stopping at the stop value
+    function generateNumbers(fizzValue, buzzValue, endValue) {
+        let numbers = [];
+        let end = endValue;
+
+        for (let value = 1; value <= end; value++) {
+            let newValue = value;
+
+            if (value % fizzValue == 0) {
+                newValue = 'Fizz'
+            }
+
+            if (value % buzzValue == 0) {
+                newValue = 'Buzz'
+            }
+
+            if (value % buzzValue == 0 && value % fizzValue == 0) {
+                newValue = 'FizzBuzz'
+            }
+
+            numbers.push(newValue);
+        }
+        return numbers;
+
+    }
+
+    //Turns the table appearance into fizz, buzz and fizz buzz
+    function displayNumbers(numbersArray) {
+        let tableBody = document.getElementById('results');
+
+        let tableHtml = "";
+
+        for (let index = 0; index < numbersArray.length; index++) {
+            let value = numbersArray[index];
+            let className = '';
+            if (value == 'Fizz') {
+                className = 'fizz';
+            }
+
+            if (value == 'Buzz') {
+                className = 'buzz';
+            }
+
+            if (value == 'FizzBuzz') {
+                className = 'fizzbuzz';
+            }
+
+            let tableRow = `<td class="${className}">${value}</td>`;
+
+            tableHtml = tableHtml + tableRow;
+
+            if ((index + 1) % 5 == 0) {
+                tableHtml += '</tr>';
+            }
+        }
+
+        tableBody.innerHTML = tableHtml;
+    }
+}
